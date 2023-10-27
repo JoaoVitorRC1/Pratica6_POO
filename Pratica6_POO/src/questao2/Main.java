cb4d9feedd62ce63858bad1d4efab6f8e20095dc
João Vitor Ribeiro <143137195+JoaoVitorRC1@users.noreply.github.com>
2023-10-27 15:39:00 -0300

    Add files via upload

---
+++ Pratica6_POO/src/questao2/Main.java
@@ -0,0 +1,18 @@
+package questao2;
+
+public class Main {
+    public static void main(String[] args) {
+        Funcionario[] listaFuncionarios = new Funcionario[5];
+        listaFuncionarios[0] = new Funcionario("Bielzin ", "bielFoot@email.com", "7777-2121", "Business Intelligence", 2300.00, "12/09/2023", "mg40228922");
+
+
+        for (Funcionario f : listaFuncionarios) {
+            f.mostrarDados();
+        }
+
+        Empresa empresa = new Empresa("Mecanica Simas Turbo Ltda", "12.345.678/0001-99");
+        for (Funcionario f : listaFuncionarios) {
+            empresa.adicionarFuncionario(f);
+        }
+    }
+}
